686682a505813257ca763cce81222da0f62ddba8
samanthachristian <smchristian@stu.naperville203.org>
2021-05-13 13:04:49 -0500

    labs

---
+++ FractleStuff - 2020/FractalTriangle.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class FractalTriangle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FractalTriangle
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class FractalTriangle
+     */
+    public FractalTriangle()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    
+    public static void drawPlolyline(int x, int y)
+
+}
